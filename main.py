80031394003ea09e68de37e1da45959ede5d7c84
Gilles Van Marcke <gilles.vanmarcke@gmail.com>
2024-12-23 12:34:49 +0100

    day 1 part 2

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
+# ------------------------ DAY 1 ----------------------
 #convert source data into separate lists
 data = pd.read_csv("input.txt", sep=" ",header=None)
 list1 = data[0].to_list()
 list2 = data[3].to_list()
-print(type(list1))
-
 
 # #sort lists
 sorted_list1 = sorted(list1)
@@ -17,8 +16,19 @@ for i in range(len(sorted_list1)):
     difference = sorted_list2[i] - sorted_list1[i]
     difference_list.append(abs(difference))
 
-
 # get sum of all numbers
 sumlist = sum(difference_list)
-print(sumlist)
 
+# -------------------- DAY 2 --------------------------
+
+# new occurences list
+occurences = []
+
+# loop over list one and count occurences, multiply the number with the occurences and save it in the new list
+for i in range(len(list1)):
+    occurencemultiplied = list1[i] * list2.count(list1[i])
+    occurences.append(occurencemultiplied)
+
+# calculate the similarity score
+similarityscore = sum(occurences)
+print(similarityscore)
